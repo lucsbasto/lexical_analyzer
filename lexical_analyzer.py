56a4fc4e098f053c7384c06bc8dcc339f953e925
Lucas Bastos <lucas.alves.lab@gmail.com>
2019-08-10 19:49:29 -0300

    adicionado json de erros

--- lexical_analyzer.py
+++ lexical_analyzer.py
@@ -9,7 +9,7 @@ terminador = [';']
 numeros = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 identificadores = ['i','j']
 tokens = []
-
+erros = []
 for i in range(len(linha)):
     if(linha[i] is not " "):
         token += linha[i]
@@ -58,6 +58,11 @@ for i in range(len(linha)):
                 "posicao": ("({},{})".format(0, i - len(token)))
             })
             token = ""
+        else:
+            erros.append({
+                "token": token,
+                "posicao": ("({},{})".format(0, i - len(token)))
+            })
         token = ""
 
     if(token in terminador):
@@ -68,12 +73,21 @@ for i in range(len(linha)):
             "posicao": ("({},{})".format(0, i - len(token)))
         })
         token = ""
+
 input.close()
 
 with open('output.txt', 'w+') as output:
+    output.write("token: {\n")
     for i in tokens:
         ij = json.dumps(i)
         output.write(ij+",\n")
-        print(i)
+    output.write("}\n")
+
+    output.write("erros: {\n")
+    for i in erros:
+        ij = json.dumps(i)
+        output.write(ij + ",\n")
+    output.write("}")
+
 output.close()
 
